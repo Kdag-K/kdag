67b4db6b7f69efa2e7f75cf1d388290be7c70ee8
xiaoming <xiaoming@new4g.cn>
2020-04-28 15:53:40 +0800

    add interface into state and validator

--- src/node/state.go
+++ src/node/state.go
@@ -23,3 +23,61 @@ const (
 	//Suspended is initialised, but not gossipping
 	Suspended
 )
+
+// String ...
+func (s State) String() string {
+	switch s {
+	case Babbling:
+		return "Babbling"
+	case CatchingUp:
+		return "CatchingUp"
+	case Joining:
+		return "Joining"
+	case Leaving:
+		return "Leaving"
+	case Shutdown:
+		return "Shutdown"
+	case Suspended:
+		return "Suspended"
+	default:
+		return "Unknown"
+	}
+}
+
+// WGLIMIT is the maximum number of goroutines that can be launched through
+// state.goFunc
+const WGLIMIT = 20
+
+type state struct {
+	state   State
+	wg      sync.WaitGroup
+	wgCount int32
+}
+
+func (b *state) getState() State {
+	stateAddr := (*uint32)(&b.state)
+	return State(atomic.LoadUint32(stateAddr))
+}
+
+func (b *state) setState(s State) {
+	stateAddr := (*uint32)(&b.state)
+	atomic.StoreUint32(stateAddr, uint32(s))
+}
+
+// Start a goroutine and add it to waitgroup
+func (b *state) goFunc(f func()) {
+	tempWgCount := atomic.LoadInt32(&b.wgCount)
+	if tempWgCount < WGLIMIT {
+		b.wg.Add(1)
+		atomic.AddInt32(&b.wgCount, 1)
+		go func() {
+			defer b.wg.Done()
+			atomic.AddInt32(&b.wgCount, -1)
+			f()
+		}()
+	}
+}
+
+func (b *state) waitRoutines() {
+	b.wg.Wait()
+}
