1750f9f56d23168afa85b5401e6a7de99728cf28
xiaoming <xiaoming@new4g.cn>
2020-11-23 14:38:56 +0800

    update some log print

--- cmd/dummy/commands/root.go
+++ cmd/dummy/commands/root.go
@@ -125,14 +125,6 @@ func newLogger() *logrus.Logger {
 		pathMap[logrus.DebugLevel] = "dummy_debug.log"
 	}
 
-	if err == nil && config.Discard {
-		logger.Out = ioutil.Discard
-	}
-
-	logger.Hooks.Add(lfshook.NewHook(
-		pathMap,
-		&logrus.TextFormatter{},
-	))
 
 	return logger
 }
