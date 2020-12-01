3d533572e64444577562b319eb738960f7453308
xiaoming <xiaoming@new4g.cn>
2020-12-01 16:17:01 +0800

    remove proxyname for test

--- cmd/dummy/commands/config.go
+++ cmd/dummy/commands/config.go
@@ -5,7 +5,6 @@ type CLIConfig struct {
 	Name       string `mapstructure:"name"`
 	ClientAddr string `mapstructure:"client-listen"`
 	ProxyAddr  string `mapstructure:"proxy-connect"`
-        ProxyName  string `mapstructure:"proxy-name"`
 	Discard    bool   `mapstructure:"discard"`
 	LogLevel   string `mapstructure:"log"`
 }
