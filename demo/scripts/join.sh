0a35e667a803a48546dd0fdb46f1a5936086a0e6
xiaoming <xiaoming@new4g.cn>
2020-12-07 17:19:23 +0800

    update test ip addr

--- demo/scripts/join.sh
+++ demo/scripts/join.sh
@@ -20,33 +20,33 @@ cp $PWD/../src/net/signal/wamp/test_data/cert.pem $dest/cert.pem
 
 # get genesis.peers.json
 echo "Fetching peers.genesis.json from node1"
-curl -s http://172.77.5.1:80/genesispeers > $dest/peers.genesis.json
+curl -s http://179.23.12.1:80/genesispeers > $dest/peers.genesis.json
 
 # get up-to-date peers.json
 echo "Fetching peers.json from node1"
-curl -s http://172.77.5.1:80/peers > $dest/peers.json
+curl -s http://179.23.12.1:80/peers > $dest/peers.json
 
 # start the new node
-docker run -d --name=client$N --net=kdagnet --ip=172.77.10.$N -it Kdag-K/dummy:latest \
+docker run -d --name=client$N --net=kdagnet --ip=179.23.12.$N -it Kdag-K/dummy:latest \
     --name="client $N" \
-    --client-listen="172.77.10.$N:1339" \
-    --proxy-connect="172.77.5.$N:1338" \
+    --client-listen="179.23.12.$N:1339" \
+    --proxy-connect="179.23.12.$N:1338" \
     --discard \
     --log="debug" 
 
-docker create --name=node$N --net=kdagnet --ip=172.77.5.$N Kdag-K/kdag:latest run \
+docker create --name=node$N --net=kdagnet --ip=179.23.12.$N Kdag-K/kdag:latest run \
     --heartbeat=100ms \
     --moniker="node$N" \
     --cache-size=50000 \
-    --listen="172.77.5.$N:1337" \
-    --proxy-listen="172.77.5.$N:1338" \
-    --client-connect="172.77.10.$N:1339" \
-    --service-listen="172.77.5.$N:80" \
+    --listen="179.23.12.$N:1337" \
+    --proxy-listen="179.23.12.$N:1338" \
+    --client-connect="179.23.12.$N:1339" \
+    --service-listen="179.23.12.$N:80" \
     --fast-sync=$FASTSYNC \
     --log="debug" \
     --sync-limit=100 \
     --webrtc=$WEBRTC \
-    --signal-addr="172.77.15.1:2443"
+    --signal-addr="179.23.12.1:2443"
 
  # --store \
 
