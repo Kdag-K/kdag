805dbe194f31f2b7417aeb7bc7c0af252e27a946
bwhour <juvemeen.xio@gmail.com>
2021-02-25 15:00:10 +0800

    update test interface

--- src/group/group_repo.go
+++ src/group/group_repo.go
@@ -1,5 +1,6 @@
 package group
 import (
+	"github.com/Kdag-K/kdag/src/peers"
 	"sync"
 )
 // InmemGroupRepo implements the GroupRepo interface with an inmem
@@ -20,11 +21,36 @@ type GroupRepo interface {
 	DeleteGroup(groupID string) error
 }
 // NewInmemGroupRepo instantiates a new InmemGroupRepo
-func NewInmemGroupRepository() *InmemGroupRepo {
+func NewInmemGroupRepo() *InmemGroupRepo {
 	return &InmemGroupRepo{
 		groupsByID:    make(map[string]*Group),
 		groupsByAppID: make(map[string][]string),
 	}
+}
+
+func TestGetGroups() {
+	repo := NewInmemGroupRepo()
+
+	group1 := NewGroup(
+		"",
+		"TestGroup1",
+		"TestApp1",
+		[]*peers.Peer{
+			peers.NewPeer("pub1", "net1", "peer1"),
+		},
+	)
+
+	group2 := NewGroup(
+		"",
+		"TestGroup2",
+		"TestApp2",
+		[]*peers.Peer{
+			peers.NewPeer("pub1", "net1", "peer1"),
+		},
+	)
+
+	allGroups,_  := repo.GetAllGroups()
+
 }
 
 // GetAllGroups implements the GroupRepo interface and returns all the
