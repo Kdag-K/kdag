21647c9bf1ea5838d80d3cec15ce26e63c80d0a9
bwhour <juvemeen.xio@gmail.com>
2021-02-25 14:49:07 +0800

    add Inmemgrouprepo data struct and implement getallgroup interface

--- src/group/group_repo.go
+++ src/group/group_repo.go
@@ -1,6 +1,14 @@
 package group
 import (
+	"sync"
 )
+// InmemGroupRepo implements the GroupRepo interface with an inmem
+// map of groups. It is thread safe.
+type InmemGroupRepo struct {
+	sync.Mutex
+	groupsByID    map[string]*Group   // [group ID] => Group
+	groupsByAppID map[string][]string // [app ID] => [GroupID,...]
+}
 
 // GroupRepo defines an interface for a repository where groups can be
 // queried, added, and manipulated. It should be thread safe.
@@ -10,4 +18,13 @@ type GroupRepo interface {
 	GetGroup(groupID string) (*Group, error)
 	SetGroup(group *Group) (string, error)
 	DeleteGroup(groupID string) error
+}
+
+// GetAllGroups implements the GroupRepo interface and returns all the
+// groups
+func (igr *InmemGroupRepo) GetAllGroups() (map[string]*Group, error) {
+	igr.Lock()
+	defer igr.Unlock()
+
+	return igr.groupsByID, nil
 }
