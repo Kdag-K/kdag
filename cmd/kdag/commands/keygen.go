93ff1625c78eb44db045875407e2550af2ec0989
bwhour <juvemeen.xio@gmail.com>
2021-06-23 16:02:13 +0800

    optimize the error log information

--- cmd/kdag/commands/keygen.go
+++ cmd/kdag/commands/keygen.go
@@ -7,6 +7,7 @@ import (
 	"path"
 
 	"github.com/Kdag-K/kdag/src/crypto/keys"
+	"github.com/palantir/stacktrace"
 	"github.com/spf13/cobra"
 )
 
@@ -40,34 +41,34 @@ func AddKeygenFlags(cmd *cobra.Command) {
 
 func keygen(cmd *cobra.Command, args []string) error {
 	if _, err := os.Stat(privKeyFile); err == nil {
-		return fmt.Errorf("A key already lives under: %s", path.Dir(privKeyFile))
+		return stacktrace.NewError("A key already lives under: %s", path.Dir(privKeyFile))
 	}
 
 	key, err := keys.GenerateECDSAKey()
 	if err != nil {
-		return fmt.Errorf("Error generating ECDSA key")
+		return stacktrace.NewError("Error generating ECDSA key")
 	}
 
 	if err := os.MkdirAll(path.Dir(privKeyFile), 0700); err != nil {
-		return fmt.Errorf("Writing private key: %s", err)
+		return stacktrace.NewError("Writing private key: %s", err)
 	}
 
 	jsonKey := keys.NewSimpleKeyfile(privKeyFile)
 
 	if err := jsonKey.WriteKey(key); err != nil {
-		return fmt.Errorf("Writing private key: %s", err)
+		return stacktrace.NewError("Writing private key: %s", err)
 	}
 
 	fmt.Printf("Your private key has been saved to: %s\n", privKeyFile)
 
 	if err := os.MkdirAll(path.Dir(pubKeyFile), 0700); err != nil {
-		return fmt.Errorf("Writing public key: %s", err)
+		return stacktrace.NewError("Writing public key: %s", err)
 	}
 
 	pub := keys.PublicKeyHex(&key.PublicKey)
 
 	if err := ioutil.WriteFile(pubKeyFile, []byte(pub), 0600); err != nil {
-		return fmt.Errorf("Writing public key: %s", err)
+		return stacktrace.NewError("Writing public key: %s", err)
 	}
 
 	fmt.Printf("Your public key has been saved to: %s\n", pubKeyFile)
