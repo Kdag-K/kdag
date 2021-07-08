1bb8c8597138d4b3ecf17f2d643ab6db7f2cb9b4
bwhour <juvemeen.xio@gmail.com>
2021-07-08 13:19:56 +0800

    update some code

--- src/common/hex.go
+++ src/common/hex.go
@@ -3,15 +3,22 @@ package common
 import (
 	"encoding/hex"
 	"fmt"
+
+	"github.com/palantir/stacktrace"
 )
 
 // EncodeToString returns the UPPERCASE string representation of hexBytes with
-// the 0X prefix
+// the 0X prefix.
 func EncodeToString(hexBytes []byte) string {
 	return fmt.Sprintf("0X%X", hexBytes)
 }
 
-// DecodeFromString converts a hex string with 0X prefix to a byte slice
+// DecodeFromString converts a hex string with 0X prefix to a byte slice.
 func DecodeFromString(hexString string) ([]byte, error) {
-	return hex.DecodeString(hexString[2:])
+	hash, err := hex.DecodeString(hexString[2:])
+	if err != nil {
+		return nil, stacktrace.NewError("DecodeFromString failed, the error is %v", err) //nolint:wrapcheck
+	}
+
+	return hash, nil
 }
