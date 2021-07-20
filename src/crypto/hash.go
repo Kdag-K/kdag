968ca8fb589821b2c55660f0285777163448268d
bwhour <juvemeen.xio@gmail.com>
2021-07-20 14:12:12 +0800

    add function Ripemd160 crypto

--- src/crypto/hash.go
+++ src/crypto/hash.go
@@ -2,6 +2,8 @@ package crypto
 
 import (
 	"crypto/sha256"
+
+	"golang.org/x/crypto/ripemd160"
 )
 
 // SHA256 returns the SHA256 hash of the data.
@@ -9,7 +11,16 @@ func SHA256(data []byte) []byte {
 	hasher := sha256.New()
 	hasher.Write(data)
 	hash := hasher.Sum(nil)
+
 	return hash
+}
+
+// Ripemd160 returns the Ripemd160 hash of the data.
+func Ripemd160(bytes []byte) []byte {
+	hasher := ripemd160.New()
+	hasher.Write(bytes)
+
+	return hasher.Sum(nil)
 }
 
 // SimpleHashFromTwoHashes returns the SHA256 hash of the concatenation of left
@@ -18,5 +29,6 @@ func SimpleHashFromTwoHashes(left []byte, right []byte) []byte {
 	var hasher = sha256.New()
 	hasher.Write(left)
 	hasher.Write(right)
+
 	return hasher.Sum(nil)
 }
