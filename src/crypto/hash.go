092b62137e5ebb631c23fd7bb9a83ad9448f21b3
bwhour <juvemeen.xio@gmail.com>
2021-07-20 14:20:11 +0800

    add rimemd160 interface

--- src/crypto/hash.go
+++ src/crypto/hash.go
@@ -32,3 +32,26 @@ func SimpleHashFromTwoHashes(left []byte, right []byte) []byte {
 
 	return hasher.Sum(nil)
 }
+
+// Rimp160 Returns hash: RIMP160( SHA256( data ) )
+// Where possible, using RimpHash() should be a bit faster.
+func Rimp160(b []byte) []byte {
+	out := make([]byte, 20)
+	rimpHash(b, out[:])
+
+	return out[:]
+}
+
+func rimpHash(in []byte, out []byte) {
+	sha := sha256.New()
+	_, err := sha.Write(in)
+	if err != nil {
+		return
+	}
+	rim := ripemd160.New()
+	_, err = rim.Write(sha.Sum(nil)[:])
+	if err != nil {
+		return
+	}
+	copy(out, rim.Sum(nil))
+}
